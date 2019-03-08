b2e814ed65a7a8a455c5017343911df193c8ca74
yoshimaroghi <maroghi_yoshi@apps.ipb.ac.id>
2019-03-09 01:50:55 +0700

    add list view for products to home screen

--- app/src/main/java/com/example/ecommerceapp/HomeScreen.kt
+++ app/src/main/java/com/example/ecommerceapp/HomeScreen.kt
@@ -2,11 +2,36 @@ package com.example.ecommerceapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.widget.ArrayAdapter
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_home_screen.*
 
 class HomeScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        val brandsUrl = "http://10.0.2.2:80/EventmanAPI/fetch_brand.php"
+        var brandsList = ArrayList<String>()
+        val requestQ = Volley.newRequestQueue(this@HomeScreen)
+        val jsonAR = JsonArrayRequest(Request.Method.GET, brandsUrl, null, Response.Listener {response ->
+            for(jsonObject in 0.until(response.length())) {
+                brandsList.add(response.getJSONObject(jsonObject).getString("brand"))
+            }
+            val brandsListAdapter = ArrayAdapter(this@HomeScreen, android.R.layout.simple_list_item_1, brandsList)
+            brandsListView.adapter = brandsListAdapter
+        }, Response.ErrorListener {error ->
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setTitle("Message")
+            dialogBuilder.setMessage(error.message)
+            dialogBuilder.create().show()
+        })
+
+        requestQ.add(jsonAR)
     }
 }
