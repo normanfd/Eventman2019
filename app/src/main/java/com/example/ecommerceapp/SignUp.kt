2e410b5279a07a63841f3d8e4d0d11383a7a4362
normanfd <norman_fd@apps.ipb.ac.id>
2019-03-08 20:26:28 +0700

    update sign up activity

--- app/src/main/java/com/example/ecommerceapp/SignUp.kt
+++ app/src/main/java/com/example/ecommerceapp/SignUp.kt
@@ -3,12 +3,24 @@ package com.example.ecommerceapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUp : AppCompatActivity() {
+class  SignUp : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        SignUpBtn.setOnClickListener {
+            if (edtSignUpPassword.text.toString().equals(edtSignUpRePassword)){
+                //Registration process
+            }else{
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Message")
+                dialogBuilder.setMessage("Password Mismatch")
+                dialogBuilder.create().show()
+            }
+        }
     }
 }
