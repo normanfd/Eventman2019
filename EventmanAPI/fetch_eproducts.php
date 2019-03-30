fa523dd8b3bb58b00b0637fd7364af34b53aa3ce
yoshimaroghi <maroghi_yoshi@apps.ipb.ac.id>
2019-03-31 06:43:55 +0700

    Add API to fetch products

--- EventmanAPI/fetch_eproducts.php
+++ EventmanAPI/fetch_eproducts.php
@@ -1,3 +1,16 @@
 <?php
 	include("connect.php");
+	
+	$q = "select * from electronic_products where brand=?";
+	$fetchProducts = $connection->prepare($q);
+	$fetchProducts->bind_param("s", $_GET["brand"]);
+	$fetchProducts->execute();
+	
+	$epRes = $fetchProducts->get_result();
+	
+	$epArray = array();
+	while($row = $epRes->fetch_assoc()) {
+		array_push($epArray, $row);
+	}
+	echo json_encode($epArray);
 ?>
