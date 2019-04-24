85a16d7eab528017007be7ec01bdfadabb30c314
normanfd <norman_fd@apps.ipb.ac.id>
2019-04-24 21:15:05 +0700

    definisikan variabel yg ada di xml

---
+++ app/src/main/java/com/example/eventman2019/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.eventman2019;
+
+public class LoginActivity {
+}
--- app/src/main/java/com/example/eventman2019/MainActivity.java
+++ app/src/main/java/com/example/eventman2019/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.eventman2019;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.firebase.database.*;
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
+    private Button JoinNowButton, loginButton;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JoinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button) findViewById(R.id.main_login_btn);
+        loadingBar = new ProgressDialog(this);
+        Paper.init(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        JoinNowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
