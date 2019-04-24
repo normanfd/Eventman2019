c14550b649e30097d276b21808817e62c863985f
normanfd <norman_fd@apps.ipb.ac.id>
2019-04-24 22:43:50 +0700

    modul login

--- app/src/main/java/com/example/eventman2019/LoginActivity.java
+++ app/src/main/java/com/example/eventman2019/LoginActivity.java
@@ -1,4 +1,135 @@
 package com.example.eventman2019;
 
-public class LoginActivity {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.eventman2019.Model.Users;
+import com.google.firebase.database.*;
+import com.rey.material.widget.CheckBox;
+import io.paperdb.Paper;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText InputPhoneNumber, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+    private String ParentDbName = "Users";
+    private CheckBox chkBoxRememberMe;
+    private TextView AdminLink, NotAdminLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        InputPassword = (EditText) findViewById(R.id.login_password_input);
+        InputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        chkBoxRememberMe = (CheckBox) findViewById(R.id.remember_me_chkb);
+        AdminLink = (TextView) findViewById(R.id.admin_panel_link);
+        NotAdminLink = (TextView) findViewById(R.id.not_admin_panel_link);
+        loadingBar = new ProgressDialog(this);
+        Paper.init(this);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+        AdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginButton.setText("Login Admin");
+                AdminLink.setVisibility(View.INVISIBLE);
+                NotAdminLink.setVisibility(View.VISIBLE);
+                ParentDbName = "Admins";
+            }
+        });
+        NotAdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginButton.setText("Login");
+                AdminLink.setVisibility(View.VISIBLE);
+                NotAdminLink.setVisibility(View.INVISIBLE);
+                ParentDbName ="Users";
+            }
+        });
+    }
+
+    private void loginUser() {
+        String phone = InputPhoneNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if(TextUtils.isEmpty(phone))
+        {
+            Toast.makeText(this, "Please write your phone number", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please write your password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            loadingBar.setTitle("Login account");
+            loadingBar.setMessage("please wait, while we are check credentials..");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            AllowAccessAccount(phone, password);
+        }
+    }
+    private void AllowAccessAccount(final String phone, final String password) {
+        if(chkBoxRememberMe.isChecked()){
+//            Paper.book().write(Prevalent.UserPhoneKey,phone);
+//            Paper.book().write(Prevalent.UserPasswordKey,password);
+        }
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(ParentDbName).child(phone).exists())
+                {
+                    Users UserData = dataSnapshot.child(ParentDbName).child(phone).getValue(Users.class);
+                    if(UserData.getPhone().equals(phone)){
+                        if(UserData.getPassword().equals(password)){
+                            if(ParentDbName.equals("Admins")){
+//                                Toast.makeText(loginActivity.this, "Welcome Admin, you are Login succesfully..", Toast.LENGTH_SHORT).show();
+//                                loadingBar.dismiss();
+//                                Intent intent = new Intent(loginActivity.this,AdminCategoryActivity.class);
+//                                startActivity(intent);
+                            }else {
+//                                Toast.makeText(loginActivity.this, "Login succesfully..", Toast.LENGTH_SHORT).show();
+//                                loadingBar.dismiss();
+//                                Intent intent = new Intent(loginActivity.this,HomeActivity.class);
+//                                Prevalent.CurrentOnlineUser = UserData;
+//                                startActivity(intent);
+                            }
+                        }
+                        else{
+                            loadingBar.dismiss();
+                            Toast.makeText(LoginActivity.this, "Password is  incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else
+                {
+                    Toast.makeText(LoginActivity.this, "Account with this " + phone + " number do not exist", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/eventman2019/Prevalent/Prevalent.java
@@ -0,0 +1,9 @@
+package com.example.eventman2019.Prevalent;
+
+import com.example.eventman2019.Model.Users;
+
+public class Prevalent {
+    public static Users CurrentOnlineUser;
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey= "UserPassword";
+}
