9acdcd1f3d74d69855d0420d629c73f9d34b2668
normanfd <norman_fd@apps.ipb.ac.id>
2019-04-29 21:29:34 +0700

    add product viewholder, interface, and fixed bug

---
+++ app/src/main/java/com/example/eventman2019/ViewHolder/ProductViewHolder.java
@@ -0,0 +1,34 @@
+package com.example.eventman2019.ViewHolder;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.example.eventman2019.Interface.ItemClickListener;
+import com.example.eventman2019.R;
+
+//class ini menyimpan referensi dari view-view yang digunakan pada sebuah item direcycle view.
+//viewholder = menyimpan view2 yang nantinya akan digunakan untuk menampikan data
+//tujuannyamenghemat waktu dibandingkan dengan memakai findbyid() saat update list dengan data yang baru
+public class ProductViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView txtProductName, txtProductDescription, txtproductPrice;
+    public ImageView imageView;
+    public ItemClickListener listner;
+
+    public ProductViewHolder(@NonNull View itemView) {
+        super(itemView);
+        imageView = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductDescription = (TextView) itemView.findViewById(R.id.product_desc);
+        txtproductPrice = (TextView)itemView.findViewById(R.id.product_price);
+    }
+    //ItemClickListener import interface import com.example.ecommerce.Interface.ItemClickListener;
+    public void setItemClickListenter(ItemClickListener listner){
+        this.listner =listner;
+    }
+    @Override
+    public void onClick(View v) {
+        listner.onClick(v, getAdapterPosition(),false);
+    }
+}
