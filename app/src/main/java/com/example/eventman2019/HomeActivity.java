e031fdf6c32152b3bd265a7ff0a9b6962f50132b
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-04-30 00:06:17 +0700

    add setting activity

--- app/src/main/java/com/example/eventman2019/HomeActivity.java
+++ app/src/main/java/com/example/eventman2019/HomeActivity.java
@@ -155,8 +155,8 @@ public class HomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_categories) {
 
         } else if (id == R.id.nav_settings) {
-//            Intent intent =new Intent(HomeActivity.this, SettingActivity.class);
-//            startActivity(intent);
+            Intent intent =new Intent(HomeActivity.this, SettingsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_logout) {
             Paper.book().destroy();
             Intent intent = new Intent(HomeActivity.this, MainActivity.class);
