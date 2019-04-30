d0d88ddd8fe339cc0d292b19805eba1317c4dc5e
gilangipb <gilang_gunawan@apps.ipb.ac.id>
2019-04-30 18:20:51 +0700

    setting activity

--- app/src/main/java/com/example/eventman2019/Prevalent/Prevalent.java
+++ app/src/main/java/com/example/eventman2019/Prevalent/Prevalent.java
@@ -6,4 +6,5 @@ public class Prevalent {
     public static Users CurrentOnlineUser;
     public static final String UserPhoneKey = "UserPhone";
     public static final String UserPasswordKey= "UserPassword";
+
 }
