19be2cd59c6f6872ec4147441a5c88b8282db29a
yoshimaroghi <maroghi_yoshi@apps.ipb.ac.id>
2019-05-14 00:55:28 +0700

    Add SHA-512 hash function to Hash class

--- app/src/main/java/com/example/eventman2019/Model/Hash.java
+++ app/src/main/java/com/example/eventman2019/Model/Hash.java
@@ -1,5 +1,14 @@
 package com.example.eventman2019.Model;
+import java.security.MessageDigest;
 
 public class Hash {
-
+    public String sha512(String s) throws java.lang.Exception {
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        byte[] digest = md.digest(s.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < digest.length; i++) {
+            sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
 }
