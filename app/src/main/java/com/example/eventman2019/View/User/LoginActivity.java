bff90dfd6834ed4aba9e6e9c876dc4d29eb1dcbc
yoshimaroghi <maroghi_yoshi@apps.ipb.ac.id>
2019-05-24 11:24:28 +0700

    Add phone number validation in LoginActivity

--- app/src/main/java/com/example/eventman2019/View/User/LoginActivity.java
+++ app/src/main/java/com/example/eventman2019/View/User/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -88,6 +89,9 @@ public class LoginActivity extends AppCompatActivity {
         else if(TextUtils.isEmpty(password))
         {
             Toast.makeText(this, "Please write your password", Toast.LENGTH_SHORT).show();
+        }
+        else if(PhoneNumberUtils.isGlobalPhoneNumber(phone)) {
+            Toast.makeText(this, "Please write valid phone number", Toast.LENGTH_SHORT).show();
         }
         else{
             loadingBar.setTitle("Login account");
