efcc7799d4a097f35ebe88fd38b020e9c969df4e
yoshimaroghi <maroghi_yoshi@apps.ipb.ac.id>
2019-05-25 00:11:55 +0700

    Hash class documentation

--- app/src/main/java/com/example/eventman2019/Model/Hash.java
+++ app/src/main/java/com/example/eventman2019/Model/Hash.java
@@ -2,8 +2,10 @@ package com.example.eventman2019.Model;
 import java.security.MessageDigest;
 
 public class Hash {
+    //method yang memberikan return value berupa hash dari string input
     public static String sha512(String s) throws Exception {
         s = addSalt(s);
+        //hash string menggunakan SHA-512
         MessageDigest md = MessageDigest.getInstance("SHA-512");
         byte[] digest = md.digest(s.getBytes());
         StringBuilder sb = new StringBuilder();
@@ -13,6 +15,7 @@ public class Hash {
         return sb.toString();
     }
 
+    //method yang memberikan return value berupa string prefix salt + input string + postfix salt
     public static String addSalt(String s) throws Exception {
         return "t3TeVV" + s + "6A123M";
     }
