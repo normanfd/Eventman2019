275872a8738842e122450e35a6055fcdc0bd984d
normanfd <norman_fd@apps.ipb.ac.id>
2019-05-24 21:07:30 +0700

    model inheritance

---
+++ app/src/main/java/com/example/eventman2019/Model/Konsumsi.java
@@ -0,0 +1,10 @@
+package com.example.eventman2019.Model;
+
+public class Konsumsi extends Product{
+    public Konsumsi() {
+    }
+
+    public Konsumsi(String category, String date, String description, String image, String pid, String price, String productname, String time) {
+        super(category, date, description, image, pid, price, productname, time);
+    }
+}
--- app/src/main/java/com/example/eventman2019/Model/Konveksi.java
+++ app/src/main/java/com/example/eventman2019/Model/Konveksi.java
@@ -1,5 +1,10 @@
 package com.example.eventman2019.Model;
 
 public class Konveksi extends Product {
+    public Konveksi() {
+    }
 
+    public Konveksi(String category, String date, String description, String image, String pid, String price, String productname, String time) {
+        super(category, date, description, image, pid, price, productname, time);
+    }
 }
